126a24d17aff506b67310a17108138cce419faf4
Edouard LACROIX <edouard.lacroix.ext@groupealixia.fr>
2021-02-19 17:15:57 +0100

    init commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '1430e2e28133847cd7a1fb54bbfb90a7'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '48dc4fc7-b7ec-40e4-97e8-5eba662b19ea'
+};
