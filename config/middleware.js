a52006d0018b0c7ae81df3ceee04fe8d9d6547b3
Edouard LACROIX <edouard.lacroix.ext@groupealixia.fr>
2021-02-22 17:48:26 +0100

    adding strapi to cors

--- config/middleware.js
+++ config/middleware.js
@@ -2,7 +2,7 @@ module.exports = {
     //...
     settings: {
         cors: {
-            origin: ['http://localhost', 'https://loveat.vercel.app'],
+            origin: ['http://localhost', 'https://loveat.vercel.app', 'https://loveat-back.herokuapp.com'],
         },
     },
 };
