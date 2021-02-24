5fbb20a7d7762c74a93b8c27da055d624e682067
Edouard LACROIX <edouard.lacroix.ext@groupealixia.fr>
2021-02-24 11:12:32 +0100

    cors fix

--- config/middleware.js
+++ config/middleware.js
@@ -2,6 +2,7 @@ module.exports = {
     //...
     settings: {
         cors: {
+            enabled: true,
             origin: ['http://localhost', 'https://loveat.vercel.app', 'https://loveat-back.herokuapp.com', '*'],
         },
     },
