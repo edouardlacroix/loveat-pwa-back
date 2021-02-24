5ff09f96fe7c3a189c2901816fafa1cd72c5bf77
Edouard LACROIX <edouard.lacroix.ext@groupealixia.fr>
2021-02-24 10:37:27 +0100

    dynamic port

--- config/server.js
+++ config/server.js
@@ -1,6 +1,6 @@
 module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1337),
+  port: env.int('PORT', process.env.PORT || 1337),
   admin: {
     auth: {
       secret: env('ADMIN_JWT_SECRET', '1430e2e28133847cd7a1fb54bbfb90a7'),
